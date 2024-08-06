c19b3e099ae9b31515ab11e11f964f9f5e8abbb5
abssha <146874137+abssha@users.noreply.github.com>
2024-08-06 21:48:44 +0530

    Flipkart

--- JS/index2.js
+++ JS/index2.js
@@ -24,6 +24,7 @@ var h_div_top=create('div');
 var h_div_bottom=create('div');
 var h_div=create('div');
 var h_div1=create('div');
+var h_div2=create('div');
 var h_a_top=create('a');
 var img=create('img');
 var h_input=create('input');
@@ -49,7 +50,8 @@ a3_span.innerHTML="Become a Seller";
 
 h_div_top.className="m-0 row";
 h_div_bottom.className="row";
-h_div.className="p-0 col-sm-12 col-lg-7 col-xl-8";
+h_div2.className="p-0 col-sm-12 col-lg-2 col-xl-2";
+h_div.className="p-0 col-sm-12 col-lg-5 col-xl-6";
 h_div1.className="p-0 col-sm-12 col-lg-5 col-xl-4";
 h_a1.setAttribute('href','#');
 h_a2.setAttribute('href','#');
@@ -73,10 +75,10 @@ a4_img.setAttribute('src','./IMG/verticalDots.svg');
 h_div_top.append(h_input);
 h_a_top.append(img);
 h_button.append(i);
-h_div.append(h_a_top);
 h_div.append(h_button);
 h_div.append(h_input);
 h_div.append(hr);
+h_div2.append(h_a_top);
 h_a1.append(a1_img);
 h_a1.append(a1_span);
 h_a1.append(a1_img2);
@@ -89,6 +91,7 @@ h_div1.append(h_a1);
 h_div1.append(h_a2);
 h_div1.append(h_a3);
 h_div1.append(h_a4);
+h_div_top.append(h_div2);
 h_div_top.append(h_div);
 h_div_top.append(h_div1);
 _append(h_div_top);
@@ -103,6 +106,8 @@ _style(h_div_top).minHeight="65px";
 _style(h_div_top).background="#fff";
 _style(h_div_top).display="flex";
 
+h_a_top.style.marginLeft="60px";
+
 _style(h_div).display="flex";
 //_style(h_div).border="1px solid orange";
 // _style(h_div).width="970px";
@@ -114,7 +119,7 @@ _style(i).fontSize="18px";
 _style(i).color="#666";
 
 _style(h_button).margin="10px 0px";
-_style(h_button).marginLeft="30px";
+// _style(h_button).marginLeft="950px";
 _style(h_button).paddingLeft="10px";
 _style(h_button).background="transparent";
 _style(h_button).border="none";
@@ -160,9 +165,46 @@ h_a4.style.paddingLeft="40px";
 
 function myFun()
 {
-    if(query4.matches)
+    if(query6.matches)
     {
-        h_input.style.width="320px";
+        h_input.style.width="270px";
+        h_a_top.style.margin="0px 45px";
+        h_div2.style.boxShadow="0px 1px 1px 0px rgba(0,0,0,0.5)";
+        h_button.style.marginLeft="10px";
+        h_input.style.marginRight="10px";
+        h_input.style.fontSize="16px";
+        h_button.style.fontSize="16px";
+        h_a1.style.paddingLeft="20px";
+        h_a1.style.fontSize="14px";
+        a1_img.style.height="19px";
+        a1_span.style.paddingLeft="2px";
+        a1_img2.style.height="12px";
+        a1_img2.style.paddingLeft="2px";
+        h_a2.style.paddingLeft="12px";
+        h_a2.style.fontSize="14px";
+        a2_img.style.height="19px";
+        a2_span.style.paddingLeft="2px";
+        h_a3.style.paddingLeft="12px";
+        h_a3.style.fontSize="14px";
+        a3_img.style.height="19px";
+        a3_span.style.paddingLeft="2px";
+        h_a4.style.paddingLeft="12px";
+        a4_img.style.height="19px";
+    }
+    else if(query5.matches)
+    {
+        h_input.style.width="100px";
+        h_input.style.fontSize="16px";
+        h_button.style.fontSize="16px";
+        h_a1.style.paddingLeft="70px";
+        h_a2.style.paddingLeft="20px";
+        h_a3.style.paddingLeft="20px";
+        h_a4.style.paddingLeft="20px";
+    }
+    else if(query4.matches)
+    {
+        h_input.style.width="450px";
+        h_button.style.marginLeft="100px";
         h_input.style.fontSize="16px";
         h_button.style.fontSize="16px";
         h_button.style.marginLeft="8px";
@@ -213,6 +255,8 @@ var query1=window.matchMedia("(max-width : 1355px)");
 var query2=window.matchMedia("(max-width : 992px)");
 var query3=window.matchMedia("(max-width : 867px)");
 var query4=window.matchMedia("(max-width : 695px)");
+var query5=window.matchMedia("(max-width : 570px)");
+var query6=window.matchMedia("(max-width : 331px)");
 
 myFun();
 
@@ -229,6 +273,14 @@ query3.addEventListener("change",function()
     myFun();
 });
 query4.addEventListener("change",function()
+{
+    myFun();
+});
+query5.addEventListener("change",function()
+{
+    myFun();
+});
+query6.addEventListener("change",function()
 {
     myFun();
 });
